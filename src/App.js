00fbf04d5247ddee96b15cdf9b5df3bf07aca270
Oliver-Iman <oliveriman255@gmail.com>
2024-05-13 10:31:05 -0500

    Inyeccion de Librerias y conexion con el backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+import Topbar from "./scenes/global/Topbar";
+import Pagos from "./scenes/form/Pagos";
+import SignUp from "./cognito/CognitoConfiguration";
+import Consulta from "./scenes/TablaConsulta/consulta";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar IsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/Pagos" element={ <Pagos/>} />
+              <Route path="/login" element={<SignUp/>}/>
+              <Route path="/consulta" element={<Consulta/>}/>
+              {/* Agrega otras rutas del dashboard aquí */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
